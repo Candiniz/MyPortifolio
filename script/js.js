e4dd50190c4c94c422e6528b82b874aa680f1cec
Candiniz <andersoncd123@gmail.com>
2023-03-13 23:06:18 +0000

    Atualização

--- script/js.js
+++ script/js.js
@@ -206,3 +206,16 @@ const mobileNavbar = new MobileNavbar(
   ".navbar-items li",
 );
 mobileNavbar.init();
+
+const observer = new IntersectionObserver   ((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
