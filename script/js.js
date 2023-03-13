d8f06d83613eaeb5a579b52e0b72780560ab473e
Candiniz <andersoncd123@gmail.com>
2023-03-13 17:21:27 +0000

    Responsive Update

--- script/js.js
+++ script/js.js
@@ -176,3 +176,33 @@ window.addEventListener('scroll', function() {
   let blurValue = offset * 0.02;
   parallaxFE.style.filter = 'blur(' + blurValue + 'px)';
 });
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+  }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick)
+  }
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".navbar-items",
+  ".navbar-items li",
+);
+mobileNavbar.init();
