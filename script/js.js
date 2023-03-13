10bb1c4e617e48d43b936cd204fdc4894e08c3cd
Candiniz <andersoncd123@gmail.com>
2023-03-13 23:01:14 +0000

    About Me Update

--- script/js.js
+++ script/js.js
@@ -176,3 +176,46 @@ window.addEventListener('scroll', function() {
   let blurValue = offset * 0.02;
   parallaxFE.style.filter = 'blur(' + blurValue + 'px)';
 });
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+  }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick)
+  }
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".navbar-items",
+  ".navbar-items li",
+);
+mobileNavbar.init();
+
+const observer = new IntersectionObserver   ((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
